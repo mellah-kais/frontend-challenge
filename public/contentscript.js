2b3148cdf8ed72d9c49b53307a4a7edec36e8757
mellah <mallahkais@gmail.com>
2022-06-11 04:28:19 +0100

    finished most of the main tasks didn't reach the option one in two hours

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,11 +1,53 @@
-// console.log('Contentscript injected');
+console.log('Contentscript injected');
 
-// let element = document.createElement('div');
-// const injectedText = document.createTextNode(`Extension-like element`);
+let budgetText = document.querySelector('.makeStyles-btb-27').textContent;
+let element = document.createElement('div');
+const injectedText = document.createTextNode(`Budget-to-Beat:${budgetText}`);
 
-// element.appendChild(injectedText);
+element.appendChild(injectedText);
+element.style.cssText = `
+      background-color: #0b226b;
+      color: white;
+      padding: 20px 20px 20px 50px;
+      text-align: center;
+      border-radius: 5px;
+      position:absolute;
+      font-size:16px;
+      top: 35px;
+      right: 50px;
+      font-weight:bold;
+      background-image: url('./images/favicon-32x32.png');
+      background-repeat:no-repeat;
+      background-position-x: 10px;
+      background-position-y: center;
+`;
 
-// const parentElement = document.querySelector(
-//   '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
-// );
-// parentElement.appendChild(element);
+let innerElement = document.createElement('div');
+const innerInjectedText = document.createTextNode(
+  `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters`,
+);
+innerElement.classList.add('modal');
+innerElement.style.cssText = `
+      display: none;
+      position: absolute;
+      top: 35px;
+      right: 50px;
+      width:200px;
+      height:300px;
+      background-color:white;
+      padding:20px;
+`;
+innerElement.appendChild(innerInjectedText);
+element.addEventListener('mouseover', function () {
+  innerElement.style.display = 'block';
+});
+element.addEventListener('mouseleave', function () {
+  innerElement.style.display = 'none';
+});
+
+const parentElement = document.querySelector(
+  '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section ',
+);
+
+parentElement.prepend(element);
+parentElement.prepend(innerElement);
--- src/views/user/UserDashboardView.tsx
+++ src/views/user/UserDashboardView.tsx
@@ -1,4 +1,5 @@
 import star from '@assets/img/star.svg';
+import sustainability from '@assets/img/sustainability.svg';
 import { GeneralOverviewCard } from '@components/user/dashboard/GeneralOverviewCard';
 import { MonthlyBarOverview } from '@components/user/dashboard/MonthlyBarOverview';
 import { Grid, Theme, useTheme } from '@mui/material';
@@ -87,7 +88,17 @@ export const UserDashboardView = (): ReactElement => {
             totalUser={totalRewards.total_rewards_user}
           />
         </Grid>
-        <Grid item xs={12} md={6}></Grid>
+        <Grid item xs={12} md={6}>
+          <GeneralOverviewCard
+            cardTitle="Total CO2 Savings"
+            colors={[theme.custom.palette.graphColorTwo.main, theme.custom.palette.graphColorTwo.light]}
+            unit="P"
+            icon={sustainability}
+            monthlyUser={totalRewards.dateframe_co2_user}
+            monthlyCompany={totalRewards.dateframe_co2_company}
+            totalUser={totalRewards.total_co2_user}
+          />
+        </Grid>
         <Grid item xs={12} md={6}>
           <MonthlyBarOverview
             cardTitle="Reward History"
